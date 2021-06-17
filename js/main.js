6535beb09ac304d89dfc4272ac91e86bcafab08f
tata98 <taval16@freeuni.edu.ge>
2021-06-17 21:43:58 +0400

    about us, promo packages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var swiper = new Swiper(".mySwiper", {
+  AOS.init();
+  
+  var swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 2000,
   },
@@ -23,3 +25,7 @@ var swiper = new Swiper(".mySwiper", {
       },
     },
   });
+
+
+
+
