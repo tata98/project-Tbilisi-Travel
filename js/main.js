e40c4060676befa05d0634d43121f6c5c15124bc
tata98 <taval16@freeuni.edu.ge>
2021-06-17 11:28:08 +0400

    index page

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 2000,
+  },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
