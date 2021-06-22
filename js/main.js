c35a6acc80b65dd8b86e4d1b8079fe2c1d65ec3c
tata98 <taval16@freeuni.edu.ge>
2021-06-22 15:36:03 +0400

    final commit

--- js/main.js
+++ js/main.js
@@ -1,43 +1,45 @@
-  AOS.init();
-  
-  var swiper = new Swiper(".mySwiper", {
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 2000,
   },
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
+
+$(function () {
+  $(".carousel-indicators .active ").click(function (e) {
+    e.preventDefault();
+    $(".carousel-inner .carousel-item img").attr('src', $(this).attr("data-img"));
   });
 
-
-  $(function () {
-    $(".carousel-indicators .active ").click(function (e) { 
-      e.preventDefault();
-      $(".carousel-inner .carousel-item img").attr('src', $(this).attr("data-img"));
-    });
-
-    $(".add-info").click(function (e) { 
-      e.preventDefault();
-      $(".add-info.active").removeClass("active");
-      $(this).addClass("active");
-     
-    });
-    
+  $(".add-info").click(function (e) {
+    e.preventDefault();
+    $(".add-info.active").removeClass("active");
+    $(this).addClass("active");
+    $(".section-content").text($(this).text());
+    $(".show-section").removeClass("show-section");
+    $("."+$(this).attr('data-id')).addClass("show-section");
   });
+
+});
